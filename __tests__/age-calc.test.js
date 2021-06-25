48a7d1e95885108d510be31e6267b81a9d342345
masha <mashanechmo@gmail.com>
2021-06-25 14:56:53 -0700

    add and fail second test

--- __tests__/age-calc.test.js
+++ __tests__/age-calc.test.js
@@ -5,4 +5,9 @@ describe('Years', () => {
     testAge.mercuryCount()
     expect(testAge.mercuryYears).toEqual(7.199999999999999);
   });
+  test('Should correctly calculate user`s age in venus years', () => {
+    let testAge = new Years(30);
+    testAge.venusCount()
+    expect(testAge.venusYears).toEqual(9);
+  });
 });
