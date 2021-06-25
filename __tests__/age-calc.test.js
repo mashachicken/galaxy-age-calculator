7763c633425ec24345c5ebad5f8cabfcb2d23d7c
masha <mashanechmo@gmail.com>
2021-06-25 14:54:24 -0700

    commit specs

---
+++ __tests__/age-calc.test.js
@@ -0,0 +1,8 @@
+import { Years} from './../src/calculator.js';
+describe('Years', () => {
+  test('Should correctly calculate user`s age in mercury years', () => {
+    let testAge = new Years(30);
+    testAge.mercuryCount()
+    expect(testAge.mercuryYears).toEqual(7.199999999999999);
+  });
+});
