fa7750e33f31520c3f337c29665e9708c20647e0
masha <mashanechmo@gmail.com>
2021-06-25 14:04:55 -0700

    add UI logic to display output to user

--- src/main.js
+++ src/main.js
@@ -1,17 +1,27 @@
 import $ from 'jquery';
 import 'bootstrap';
 import { Years } from './../src/calculator.js';
-$(document).ready(function(){
-  $("#calculateYearsForm").submit(function(event) {
+$(document).ready(function () {
+  $("#calculateYearsForm").submit(function (event) {
     event.preventDefault()
     const userAge = $("#solar").val();
     const lifeExpectancy = $("#life").val();
-    let userYears = new Years (userAge, lifeExpectancy)
+    let userYears = new Years(userAge, lifeExpectancy)
+    userYears.mercuryCount()
+    userYears.venusCount()
+    userYears.marsCount()
+    userYears.jupiterCount()
 
-    userYears.yearsLeft().forEach(function(year){
+    userYears.yearsLeft().forEach(function (year) {
       if (year >= 0) {
         $("#yearsleft").append("<li>" + year + "</li>")
       } else {
         $("#yearspast").append("<li>" + year * (-1) + " years pass the expectancy" + "</li>")
-      };
-    })
+      }
+    });
+      $(".mercury").html(userYears.mercuryYears);
+      $(".venus").html(userYears.venusYears);
+      $(".mars").html(userYears.marsYears);
+      $(".jupiter").html(userYears.jupiterYears);
+    });
+  });
